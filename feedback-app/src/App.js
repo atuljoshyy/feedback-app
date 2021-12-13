fd483fe2649d10fc56f93f89be5e74d685d5de10
AtulJoshyy <atuljoshyy@gmail.com>
2021-12-13 09:47:17 +0530

    added css 🚀

---
+++ feedback-app/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+function App() {
+  return (
+    
+    <div className='container'>
+        <Header />
+      <h1>My App</h1>
+    </div>
+    
+  );
+}
+
+export default App;
